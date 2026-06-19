bb59b505bacbe4fbae5028684f2d4bed05b9e292
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2026-06-20 00:54:47 +0300

    refactor: update tsconfig.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { provideHttpClient, withXhr } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+import { type ApplicationConfig } from '@angular/core';
 
 import { App } from './app/app';
 import { routes } from './app/routes';
